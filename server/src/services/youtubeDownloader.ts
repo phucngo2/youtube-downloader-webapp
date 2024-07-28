580551cce62b41fb78b97ebff15014322fa27a76
phucngo2 <phucnnh21@gmail.com>
2024-07-28 22:23:18 +0700

    Fix: Update dependencies

--- server/src/services/youtubeDownloader.ts
+++ server/src/services/youtubeDownloader.ts
@@ -1,27 +1,27 @@
-const ytdl = require("ytdl-core");
+const ytdl = require("@distube/ytdl-core");
 
 export class YoutubeDownloader {
-    public static async listKeywords(url: string) {
-        const video = await ytdl.getInfo(url);
-        const keywords = video.videoDetails.keywords;
-        return keywords;
-    }
+  public static async listKeywords(url: string) {
+    const video = await ytdl.getInfo(url);
+    const keywords = video.videoDetails.keywords;
+    return keywords;
+  }
 
-    public static async listFormats(url: string) {
-        const video = await ytdl.getInfo(url);
+  public static async listFormats(url: string) {
+    const video = await ytdl.getInfo(url);
 
-        return {
-            videoUrl: video.videoDetails.video_url,
-            formats: video.formats,
-            thumbnail: video.videoDetails.thumbnails.at(-1).url,
-            title: video.videoDetails.title,
-            keywords: video.videoDetails.keywords,
-        };
-    }
+    return {
+      videoUrl: video.videoDetails.video_url,
+      formats: video.formats,
+      thumbnail: video.videoDetails.thumbnails.at(-1).url,
+      title: video.videoDetails.title,
+      keywords: video.videoDetails.keywords,
+    };
+  }
 
-    public static async downloadVideo(url: string, quality: string) {
-        return ytdl(url, {
-            quality: quality,
-        });
-    }
+  public static async downloadVideo(url: string, quality: string) {
+    return ytdl(url, {
+      quality: quality,
+    });
+  }
 }
